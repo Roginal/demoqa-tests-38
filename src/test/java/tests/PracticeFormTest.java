4e469677f2dd2dba84bbd6a81b9d66f884952e58
Plekhov Gleb <plekhov.gleb@yandex.ru>
2025-10-19 23:37:47 +0300

    added PracticeFormTest

---
+++ src/test/java/tests/PracticeFormTest.java
@@ -0,0 +1,61 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class PracticeFormTest {
+
+    @BeforeAll
+    public static void setupEnvironment(){
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+
+    }
+
+    @Test
+    void fillFormTest() {
+        open("/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+
+        $("#firstName").setValue("Gleb");
+        $("#lastName").setValue("Plekhov");
+        $("#userEmail").setValue("plekhov.gleb@yandex.ru");
+        $("#genterWrapper").$(byText("Male")).click();
+        $("#userNumber").setValue("7878787878");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("April");
+        $(".react-datepicker__year-select").selectOption("1996");
+        $(".react-datepicker__day--006:not(.react-datepicker__day--outside-month)").click();
+        $("#subjectsInput").setValue("Hindi").pressEnter();
+        $("#hobbiesWrapper").$(byText("Reading")).click();
+        $("#uploadPicture").uploadFromClasspath("img/pic1.jpg");
+        $("#currentAddress").setValue("Street 123");
+        $("#react-select-3-input").setValue("NCR").pressEnter();
+        $("#react-select-4-input").setValue("Delhi").pressEnter();
+        $("#submit").click();
+
+        $(".modal-dialog").should(appear);
+
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text("Gleb Plekhov"));
+        $(".table-responsive").shouldHave(text("plekhov.gleb@yandex.ru"));
+        $(".table-responsive").shouldHave(text("Male"));
+        $(".table-responsive").shouldHave(text("Male"));
+        $(".table-responsive").shouldHave(text("7878787878"));
+        $(".table-responsive").shouldHave(text("06 April,1996"));
+        $(".table-responsive").shouldHave(text("Hindi"));
+        $(".table-responsive").shouldHave(text("Reading"));
+        $(".table-responsive").shouldHave(text("pic1.jpg"));
+        $(".table-responsive").shouldHave(text("Street 123"));
+        $(".table-responsive").shouldHave(text("NCR Delhi"));
+        $("#closeLargeModal").click();
+    }
+}
